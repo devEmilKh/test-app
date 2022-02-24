24fb5a45e3b189069d73b0017c2fdd4d8f757e02
Emil Khayretdinov <emilkh89@gmail.com>
2022-02-24 14:26:30 +0300

    изменения

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 import '../index.css';
 import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme, GlobalStyles} from "./theme";
-import logo from '../assets/Frame227.png'
-import Data from './Data'
+import logo from '../assets/Frame227.png';
+import Data from './Data';
+import VectorFD from '../assets/VectorFD.png';
+import VectorFL from '../assets/VectorFL.png';
 
 function App() {
   const [theme, setTheme] = useState("dark");
+  const [vector, setVector] = useState(VectorFD);
+  const changeButton = () => {
+    vector === VectorFL ? setVector(VectorFD) : setVector(VectorFL);
+  };
   const switchTheme = () => {
     theme === "light" ? setTheme("dark") : setTheme("light");
 };
@@ -17,7 +23,7 @@ return (
       <GlobalStyles />
           <div className="Frame">
             <img className="logo" src={ logo } alt="logo"/>
-            <img className="button" onClick={ switchTheme } src= {logo} alt="button" />
+            <img className="button" onClick={ switchTheme } src= {changeButton} alt="button" />
           </div>
           <div>
               <Data />
